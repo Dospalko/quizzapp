3142b84a4b0064dae14c303a37bc794aa007700c
Dominik Paľo <dominik.palo@student.tuke.sk>
2023-09-04 13:12:03 +0200

    new l

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,10 @@
-
+"use client";
 import Navbar from "./components/Navbar";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import { AuthContextProvider } from "./context/AuthContext";
 const inter = Inter({ subsets: ["latin"] });
+
 
 
 export default function RootLayout({ children }) {
